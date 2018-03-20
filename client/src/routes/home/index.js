c502520fbf7709bede247b50158809e93ba7a1e0
Simon 徐 <simon@sc.10086.cn.defaultbadlist>
2018-03-20 22:31:53 +0800

    123

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from 'material-ui/Button';
-
+import Paper from 'material-ui/Paper';
 import Dialog, {
   DialogTitle,
   DialogContent,
@@ -33,10 +33,59 @@ class Home extends React.Component {
         });
     };
     render(){
+        const style = {
+            height: 100,
+            width: 100,
+            margin: 20,
+            textAlign: 'center',
+            display: 'inline-block',
+          };
         const { open } = this.state;
         return (
             <div >
               <AppBanner />
+              <div style={
+                  {
+                      background: "black",
+                      color: "white",
+                      position: "relative",
+                      top: "-120px",
+                      height: "100%"
+                  }
+              }>
+              <div >
+                  <h3>什么是wmv?</h3>
+                <Paper style={style} zDepth={1} />
+                <Paper style={style} zDepth={2} />
+                
+            </div>
+            <div>
+                  <h3>我们该如何使用wmv?</h3>
+                <Paper style={style} zDepth={1} />
+                <Paper style={style} zDepth={2} />
+                
+            </div>
+            <div>
+                  <h3>WMV的优势?</h3>
+                <Paper style={style} zDepth={1} />
+                <Paper style={style} zDepth={2} />
+               
+            </div>
+            <div>
+                  <h3>团队介绍?</h3>
+                <Paper style={style} zDepth={1} circle={true} />
+                <Paper style={style} zDepth={2} circle={true} />
+                
+            </div>
+            <div id="footer">
+                    <h4>联系我们</h4>
+                  <Paper style={style} zDepth={1} circle={true} >
+            
+                  </Paper>
+                <Paper style={style} zDepth={2} circle={true} />
+                
+            </div>
+            </div>
              
           </div>
         )
